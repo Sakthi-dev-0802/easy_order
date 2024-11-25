0e90ee80db1f44379919564f5e172b1849f1d306
Sakthi_08 <sakthi.dev0802@gmail.com>
2024-11-25 08:07:21 +0530

    firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:easy_order/firebase_options.dart';
 import 'package:easy_order/routes/router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
